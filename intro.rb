29c61b596bbf0f5cf4f0b721a5bb50919b1d9f8b
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-08-12 13:16:44 +0530

    intro to ruby - print and datatypes

---
+++ intro.rb
@@ -0,0 +1,60 @@
+# PRINT: Puts - next line, Print - same line
+puts "hello world!"
+print "heyy"
+
+# VARIABLES
+name = "Anshika"
+puts "Hello #{name} " + name
+
+# DATA TYPES
+## numbers
+time = 60
+dist = 7.8
+
+## bool
+if(0) 
+  puts "0 is truthy"
+else 
+  puts "0 is falsey"
+end 
+
+if(nil) 
+  puts "true"
+else 
+  puts "only nil and false are falsey"
+end
+
+## strings
+greet = "Welcome"
+name = "Anshika"
+puts "using double quotes: \n" + "#{greet}, #{name}. Let's meet in c:\\books"
+puts 'using single quotes: \n' + '#{greet}, #{name}. Let\'s meet'
+
+## hashes (object in js)
+name_age = {"a" => 20, "b" => 30, "c" => 14, "d" => 17,};
+name_age.each do |k, v|
+  puts "Name: #{k}, age: #{v}"
+end
+
+## array
+arr = [1, "string", 4.9, true, {"key1" => "value1", "key2" => "value2"}]
+arr.each do |i|
+  puts i
+end
+
+## symbols: light-weight strings
+=begin
+when we use string, let's say 'name', ruby creates a new object every time we use it
+but when we use symbol, let's say ':name', ruby reuses it
+=end
+
+hash_symbol = {
+  :a => "apple",
+  :b => "banana",
+  :c => "citrus",
+  :d => "dragon fruit",
+}
+
+hash_symbol.each do |key, value|
+  puts "#{key} - #{value}"
+end
