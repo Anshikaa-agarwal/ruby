8ba7c5b1620c706fe16fb21c3976757d49e155c5
Anshikaa-agarwal <anshika2020ag@gmail.com>
2025-08-13 10:51:12 +0530

    yield in rb

--- yield.rb
+++ yield.rb
@@ -11,3 +11,22 @@ end
 puts "End of method"
 end
 call_block {puts "hello"}
+
+
+# way2
+class Wish
+  def greet
+    if block_given?
+      yield("Anshika")
+    else 
+      puts "No user found"
+    end
+  end
+end
+
+w1 = Wish.new()
+w1.greet {|user| puts "Good morning, #{user}"}
+
+w2 = Wish.new()
+w2.greet
+    
